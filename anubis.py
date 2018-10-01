0b0758c8d2591f0cdb7ae1d3848ce13dc3c995d1
th3-j4ck4l <maha678@icloud.com>
2018-10-02 00:23:47 +0300

    amelasyonUpdate

--- anubis.py
+++ anubis.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 from os import popen, chdir, system
 import base64
 import sys
-
+from glob import glob
+from typing import List
 '''
 python3 anubis.py '~/platform-tools/adb' '~/apk/d2/' 'anubis.bot.myapplication'
 '''
@@ -12,17 +13,13 @@ python3 anubis.py '~/platform-tools/adb' '~/apk/d2/' 'anubis.bot.myapplication'
 '''
 req: https://github.com/CyberSaxosTiGER/androidDump
 '''
-from pprint import pprint
-import os
-FILE_LIST = []
-
 def swap(i, i2, arr):
     i3 = arr[i]
     arr[i] = arr[i2]
     arr[i2] = i3
 
 
-def solve(key, encoded):
+def solve(key:str, encoded:str) -> List:
     t = [i for i in range(256)]
 
     c = i2 = 0
@@ -48,61 +45,60 @@ def solve(key, encoded):
     return ''.join([chr(x) for x in barr4])
 
 
-def getkey(filename):
+def getkey(filename:str) -> List(str):
     rt = []
-    pivot = popen('grep -rnai https://twitter ' + filename + '-out | head -1  &> /dev/null ').read().split(':')[0]
-    twitter = popen(
-        'head ' + pivot + ' -n51 | tail -n1 | sed -r  \'s/.*.(")(.*.)(").*$/\\2/\'  &> /dev/null').read()[:-1]
-    rt.append(popen(
-        'head ' + pivot + ' -n57 | tail -n1 | sed -r  \'s/.*.(")(.*.)(").*$/\\2/\'  &> /dev/null').read()[:-1])
+    pivot = popen('grep -rnai https://twitter ' + filename +
+                  '-out | head -1 ').read().split(':')[0]
+    twitter = open(pivot).read().split('\n')[50].split('"')[1]
+    rt.append(open(pivot).read().split('\n')[56].split('"')[1])
     response = requests.get(twitter)
     rt.append(twitter)
     soup = BeautifulSoup(response.text, "html.parser")
     tweets = soup.findAll('li', {"class": 'js-stream-item'})
     for tweet in tweets:
         if tweet.find('p', {"class": 'tweet-text'}):
-            rt.append(str(tweet.find('p',{"class":'tweet-text'}).text.encode('utf8').strip()).split(">")[1].split("<")[0])
+            rt.append(str(tweet.find('p', {
+                      "class": 'tweet-text'}).text.encode('utf8').strip()).split(">")[1].split("<")[0])
             break
     return rt
 
 
-def adbRun(adb,packageName):
+def adbRun(adb:str, packageName:str):
     system(adb + ' push androidDump.out /data/local/tmp')
-    system(adb + ' shell \'cd /data/local/tmp && ./androidDump.out ' + packageName +  "' &> /dev/null")
+    system(adb + ' shell \'cd /data/local/tmp && ./androidDump.out ' +
+           packageName + "' &> /dev/null")
     system(adb + ' pull /data/local/tmp')
 
-def run(d2j,fileName):
-    system(d2j + "d2j-dex2smali.sh tmp/"+ fileName +'.dex' + " > /dev/null")
-    pprint(os.getcwd())
-    fileName = popen("find -maxdepth 1 -type d -name '" + fileName + "-out' &> /dev/null ").read()[:-1][2:]
-    print(fileName)
 
-def dexExc():
-    filenames = popen("ls tmp/?????  &> /dev/null ").read().split('\n')[:-1]
+def run(d2j:str, fileName:str):
+    system(d2j + "d2j-dex2smali.sh tmp/" + fileName + '.dex' + " > /dev/null")
+    fileName = popen("find -maxdepth 1 -type d -name '" +
+                     fileName + "-out' &> /dev/null ").read()[:-1][2:]
+
+
+def dexExc() -> List(str):
+    filenames = glob('?????')
     for filename in filenames:
         with open(filename, 'rb') as f:
             content = f.read()
         hexoc = binascii.hexlify(content).split(b'6465780a')
         hexoc.pop(0)
         hexoc = b'6465780a' + b''.join(hexoc)
-        dex = open(filename+".dex","wb")
-        dex.write(binascii.a2b_hex(hexoc[:int.from_bytes(binascii.a2b_hex(hexoc[:72][-8:]),byteorder='little')*2]))
+        dex = open(filename+".dex", "wb")
+        dex.write(binascii.a2b_hex(hexoc[:int.from_bytes(
+            binascii.a2b_hex(hexoc[:72][-8:]), byteorder='little')*2]))
         dex.close()
-    pprint(filenames)
-    return [ filename.split('/')[len(filename.split('/')) - 1] for filename in filenames]
+    return [filename.split('/')[len(filename.split('/')) - 1] for filename in filenames]
 
 
 def main():
     adbPath = sys.argv[1]
     dex2jarPath = sys.argv[2]
     packageName = sys.argv[3]
-    #adbPath = '~/platform-tools/adb' 
-    #dex2jarPath = '~/apk/d2/'
-    #packageName = 'com.mhyspybnfqgl.kcseuel'
     adbRun(adbPath, packageName)
 
     for dexName in dexExc():
-        run(dex2jarPath,dexName)
+        run(dex2jarPath, dexName)
         try:
             keys = getkey(dexName)
             system("clear")
@@ -110,6 +106,7 @@ def main():
             print(solve(keys[0], keys[2]))
             break
         except:
-            pass    
+            pass
+
 
 main()
